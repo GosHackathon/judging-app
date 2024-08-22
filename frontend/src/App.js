1db99747dc83bedaa6b60251ce3db73ffbbf44d0
Faizal <112001098+mohammedfazial97@users.noreply.github.com>
2024-08-22 19:28:05 +0800

    Update App.js
    Signed-off-by: Faizal <112001098+mohammedfazial97@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,44 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./components/Login";
-import Signup from "./components/Signup"; // Import the Signup component
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./auth/Login";
+import MainJudgeLogin from "./auth/MainJudgeLogin";
+import MainJudgeSignup from "./auth/MainJudgeSignup";
+import Signup from "./auth/Signup";
 import Dashboard from "./components/Dashboard";
 import ScoreEntry from "./components/ScoreEntry";
 import Leaderboard from "./components/Leaderboard";
-import Form from "./components/Form"; // Import the Form component
+import TeamManagement from "./components/TeamManagement";
+import ProtectedRoute from "./auth/ProtectedRoute";
+import MainJudgeHome from "./components/MainJudgeHome";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} /> {/* Add Signup route */}
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/score-entry" element={<ScoreEntry />} />
-        <Route path="/leaderboard" element={<Leaderboard />} />
-        <Route path="/form" element={<Form />} /> {/* Add Form route */}
+        <Route path="/main-judge-login" element={<MainJudgeLogin />} />
+        <Route path="/main-judge-signup" element={<MainJudgeSignup />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/dashboard"
+          element={<ProtectedRoute element={<Dashboard />} />}
+        />
+        <Route
+          path="/score-entry"
+          element={<ProtectedRoute element={<ScoreEntry />} />}
+        />
+        <Route
+          path="/leaderboard"
+          element={<ProtectedRoute element={<Leaderboard />} />}
+        />
+        <Route
+          path="/main-judge-home"
+          element={<ProtectedRoute element={<MainJudgeHome />} />}
+        />
+        <Route
+          path="/team-management"
+          element={<ProtectedRoute element={<TeamManagement />} />}
+        />
       </Routes>
     </Router>
   );
