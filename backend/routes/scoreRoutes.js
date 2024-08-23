07ae32ba70cd9a9a13eafb0447f214c8cef6b95c
Dsebast <Dsebast1@our.ecu.edu.au>
2024-08-23 21:02:48 +0800

    Delete backend/routes directory
    Signed-off-by: Dsebast <Dsebast1@our.ecu.edu.au>

--- backend/routes/scoreRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const scoreController = require("../controllers/scoreController");
-
-// Route to handle score submission
-router.post("/scores", scoreController.submitScores);
-
-module.exports = router;
