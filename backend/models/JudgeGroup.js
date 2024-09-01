10b79d42f05a0ccbbfd12c7174a4758eb747bb92
Dsebast <Dsebast1@our.ecu.edu.au>
2024-09-01 13:17:35 +0800

    Add files via upload
    Signed-off-by: Dsebast <Dsebast1@our.ecu.edu.au>

---
+++ backend/models/JudgeGroup.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const judgeGroupSchema = new mongoose.Schema({
+  groupName: {
+    type: String,
+    required: true,
+    unique: true,  // Ensure unique group names
+  },
+  judges: [
+    { 
+      name: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,  // Ensure unique judge emails
+      },
+    },
+  ],
+  teams: [{
+    teamId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'UploadTeam',
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    }
+  }],
+});
+
+const JudgeGroup = mongoose.model('JudgeGroup', judgeGroupSchema);
+
+module.exports = JudgeGroup;
